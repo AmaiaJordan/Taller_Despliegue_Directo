56965b7e66e4fbf3edf4905093f67a2d3d3c996d
AMAIAJO <amaiajo1990@gmail.com>
2025-01-10 13:13:11 +0100

    primera prueba que funciona en pythonanywhere

--- app_model.py
+++ app_model.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 import numpy as np
 
-os.chdir(os.path.dirname(__file__))
+root_path= "/home/Amaia/Taller_Despliegue_Directo/"
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -21,8 +21,8 @@ def hello(): # Ligado al endopoint "/" o sea el home, con el método GET
 # Enruta la funcion al endpoint /api/v1/predict
 @app.route('/api/v1/predict', methods= ['GET'])
 def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
-    #model = pickle.load(open(root_path+'ad_model', 'rb'))
-    model = pickle.load(open('ad_model.pkl','rb'))
+    model = pickle.load(open(root_path+'ad_model.pkl', 'rb'))
+    #model = pickle.load(open('ad_model.pkl','rb'))
     tv = request.args.get('tv', None)
     radio = request.args.get('radio', None)
     newspaper = request.args.get('newspaper', None)
@@ -34,14 +34,14 @@ def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
         return "Args empty, the data are not enough to predict"
     else:
         prediction = model.predict([[float(tv),float(radio),float(newspaper)]])
-    
+
     return jsonify({'predictions': prediction[0]})
 
 # Enruta la funcion al endpoint /api/v1/retrain
 @app.route('/api/v1/retrain', methods= ['GET'])
 def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
     if os.path.exists("data/Advertising_new.csv"):
-        data = pd.read_csv('data/Advertising_new.csv')
+        data = pd.read_csv(root_path + 'data/Advertising_new.csv')
 
         X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
                                                         data['sales'],
@@ -53,10 +53,11 @@ def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
         rmse = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
         mape = mean_absolute_percentage_error(y_test, model.predict(X_test))
         model.fit(data.drop(columns=['sales']), data['sales'])
-        pickle.dump(model, open('ad_model.pkl', 'wb'))
+        pickle.dump(model, open(root_path + 'ad_model.pkl', 'wb'))
 
         return f"Model retrained. New evaluation metric RMSE: {str(rmse)}, MAPE: {str(mape)}"
     else:
         return f"<h2>New data for retrain NOT FOUND. Nothing done!</h2>"
-    
-app.run()
+
+if __name__== "__main__":
+    app.run()
